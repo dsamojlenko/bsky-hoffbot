b86d3c7387002b871b891b27d744a8a08eeee3d7
Dave Samojlenko <dsamojlenko@gmail.com>
2024-11-17 13:54:36 -0500

    Run the hoffbot

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,17 @@
 import { CronJob } from 'cron';
-import { fetchPosts } from './fetchPosts';
-import { postNewPosts } from './postNewPosts';
-import { login, agent } from './bsky/auth';
+import { login } from './bsky/auth';
+import { likeMentions } from './hoffbot/likeMentions';
 
 login()
   .then(() => {
-    // Run fetchPosts on a cron job
-    const fetchScheduleExpression = '*/10 * * * *';
-    const fetchJob = new CronJob(fetchScheduleExpression, async () => {
-      console.log('Fetching posts...');
-      await fetchPosts();
-      console.log('Finished fetching posts');
-    });
-
-    // Run postNewPosts on a separate cron job
-    const postScheduleExpression = '*/10 * * * *';
-    const postJob = new CronJob(postScheduleExpression, async () => {
-      console.log('Posting new posts...');
-      await postNewPosts(agent);
-      console.log('Finished posting new posts');
+    console.log('Starting the hoffbot...');
+    const scheduleExpression = '*/10 * * * *';
+    const likeMentionsJob = new CronJob(scheduleExpression, async () => {
+      await likeMentions();
     });
 
     // Start the cron jobs
-    fetchJob.start();
-    postJob.start();
+    likeMentionsJob.start();
   })
   .catch((err) => {
     console.error('Error during setup', err);
